f00cc224bf917661fba99ba9c9242452598a4e5c
Shaxzod <botirjonovshaxzod0@gmail.com>
2024-02-05 21:34:25 +0500

    removed code

--- js/index.js
+++ js/index.js
@@ -26,36 +26,3 @@ const scissors = `
 `;
 
 const choices = ["rock", "paper", "scissors"];
-
-const computerSelection = () => {
-  const randomNumber = Math.floor(Math.random() * 3);
-  return choices[randomNumber];
-};
-
-const playRound = function () {
-  const playerSelection = prompt(
-    "What do you choose? Rock, Paper, or Scissors."
-  ).toLowerCase();
-
-  const result = compareSelections(playerSelection, computerSelection());
-
-  console.log(result);
-};
-
-const compareSelections = function (playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
-    return `It's a tie! Both of us selected ${playerSelection}`;
-  }
-
-  if (
-    (playerSelection === "rock" && computerSelection === "scissors") ||
-    (playerSelection === "paper" && computerSelection === "rock") ||
-    (playerSelection === "scissors" && computerSelection === "paper")
-  ) {
-    return `You win! ${playerSelection} beats ${computerSelection}.`;
-  } else {
-    return `You lose! ${computerSelection} beats ${playerSelection}.`;
-  }
-};
-
-playRound();
