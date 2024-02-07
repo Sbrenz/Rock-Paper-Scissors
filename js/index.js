6c7be329fc19548b94cfeb9183a6b57cfd80a1c9
wfarre <william.farre@gmail.com>
2024-02-07 17:49:26 +0800

    add the different functions asked

--- js/index.js
+++ js/index.js
@@ -26,3 +26,26 @@ const scissors = `
 `;
 
 const choices = ["rock", "paper", "scissors"];
+let playerScore = 0;
+let computerScore = 0;
+
+/**
+ * @returns {string} corresponding to the player's selection
+ */
+const playerPlay = () => {};
+
+/**
+ * @returns {string} corresponsing to the computer's choice
+ */
+const computerPlay = () => {};
+
+/**
+ * @param {string} playerSelection
+ * @param {string} computerSelection
+ * @returns {string} the winner of the round
+ */
+const playRound = (playerSelection, computerSelection) => {};
+
+const game = () => {
+  for (let i = 0; i < 5; i++) {}
+};
